218391d1cc26592d843ab6fcc7b62b73af015c87
virgiliocantu <virgilio.cantu@gmail.com>
2019-11-15 01:05:48 -0600

    I tried a while but it looped forever. Need to fix it and make it aske for your next letter. Limited to 10 tries.

--- Letter.js
+++ Letter.js
@@ -1,41 +1,51 @@
 const inquirer = require("inquirer");
 const word = "Papalote";
 const displayWord = [];
+var notComplete = false;
+var tries = 10;
 
 // Letter {}; Ddefine the constructor
-Letter = {
-    underChar: "_",
-    guessed: false,
-    processLetter: function(findLetter) {
-        renderWord(word.length);
-        if (word.indexOf(findLetter));
-        {
-            Letter.guessed = true;
-            console.log("You guessed it!", findLetter);
-            // DO SOMETHING
+
+    Letter = {
+        underChar: "_",
+        guessed: false,
+        processLetter: function(findLetter) {        
+            if (word.indexOf(findLetter))
+            {
+                Letter.guessed = true;
+                console.log("You guessed it!", findLetter);
+            } else {
+                console.log("Bad letter. Try again!", findLetter);
+            }
+            renderWord(findLetter,word.length);
         }
     }
-}
 
 // Accept input from the user
+//while (tries>0) {
+    console.log(tries);
     inquirer
-    .prompt ([
-    {
-        name: "letter",
-        message: "PLease enter a letter:",
-    }
-    ])
-.then(function(iResp) {
-    console.log(iResp.letter);
-    var findLetter = iResp.letter;
-    Letter.processLetter(findLetter);
+        .prompt ([
+        {
+            name: "letter",
+            message: "PLease enter a letter:",
+        }
+        ])
+    .then(function(iResp) {  
+        console.log(iResp.letter);
+        var findLetter = iResp.letter;
+        Letter.processLetter(findLetter);
+    })
+    tries --;
+//}
 
-})
-
-function renderWord(size) {
+function renderWord(letter,size) {
     console.log("I made it this far: ", size);
     for (var i=0; i<size; i++) {
-        displayWord.push("_");
+        if (word[i] === letter) {
+            displayWord.push(letter);
+        } else
+            displayWord.push("_");
     }   
-    console.log(displayWord);
+    console.log(displayWord.join(" "));
 }
